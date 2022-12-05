54fd92424667baa1177c8cdb5f65ea3f79d0ee1f
precupa-2022 <precupa@educ.college-montreal.qc.ca>
2022-12-04 22:45:39 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+Programme fait par Alexander Precup
+Groupe: 402
+Description: Objets orientées
+Exercise 2 - Écrire une classe Rectangle
+"""
+
+class Rectangle():
+    def __init__(self, largeur, longueur):
+        self.largeur = largeur
+        self.longueur  = longueur
+        self.aire = 0
+
+    def calculer_aire(self):
+        return self.largeur * self.longueur
+
+    def afficher_infos(self):
+        print(f"Longueur rectangle : {self.longueur}")
+        print(f"Largeur rectangle : {self.largeur}")
+
+r = Rectangle(12, 20)
+r_aire = r.calculer_aire()
+r.afficher_infos()
+print(f"Aire rectangle : {r_aire}")
